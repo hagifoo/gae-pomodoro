a34c4e066abd5053a9bbf3bac269c68e992a87ae
hagifoo <hagiharatoshishige@gmail.com>
2017-01-16 13:58:45 +0900

    Add timer queue

--- app/src/infra/taskqueue.py
+++ app/src/infra/taskqueue.py
@@ -10,11 +10,12 @@ def add_timer_end_task(user_id, start_at, eta):
         target='default',
         name=get_timer_end_task_name(user_id, start_at),
         params={'user_id': user_id},
+        queue_name='timer',
         eta=eta
     )
 
 def delete_timer_end_task(user_id, start_at):
-    q = taskqueue.Queue()
+    q = taskqueue.Queue('timer')
     q.delete_tasks(taskqueue.Task(
         name=get_timer_end_task_name(user_id, start_at),
     ))
